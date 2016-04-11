c651b36ade26a63b459d52826324593027e0dc0d
OrcMan1020 <sai.xu@citrix.com>
2016-04-11 22:06:38 +0800

    First Commit

--- karma.conf.js
+++ karma.conf.js
@@ -4,9 +4,9 @@ module.exports = function(config){
     basePath : './',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/lib/angular/angular.js',
+      'app/lib/angular-route/angular-route.js',
+      'app/lib/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
       'app/view*/**/*.js'
     ],
