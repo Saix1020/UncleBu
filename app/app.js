32121b6576bbcb6c7c545b1a79eae6fbaeb14c89
OrcMan1020 <sai.xu@citrix.com>
2016-04-12 22:34:08 +0800

    First Commit

--- app/app.js
+++ app/app.js
@@ -3,8 +3,57 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-    'myApp.main',
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/main'});
-}]);
+  'myApp.new_product'
+])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.otherwise({redirectTo: '/new_product'});
+    }])
+
+    .service('utilFun', function(allMenuTitles){
+      this.enableMenuTitle = function(menuId){
+        var allTitles = allMenuTitles.get();
+        allTitles.forEach(function(title){
+          if(title === menuId){
+            $('#' + menuId + ' a').addClass('active');
+          }
+          else {
+            $('#' + title + ' a').removeClass('active');
+          }
+        })
+
+      };
+      //this.disableMenuTitle = function(menuId){
+      //  $('#'+menuId).removeClass('active');
+      //};
+
+      this.fadeInPage = function(page){
+        $(page).addClass("animated fadeInDown").show();
+      };
+
+    })
+    .factory('allMenuTitles', function(){
+      return {
+        get : function(){
+          return [
+            'new-product',
+            'daily-textile',
+            'wedding-textile',
+            'wedding-service',
+            'about-us',
+            'contact-us'
+          ];
+        }
+      }
+    })
+    .directive('onFinishRenderFilters', function ($timeout) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attr) {
+          if (scope.$last === true) {
+            $timeout(function() {
+              scope.$emit('ngRepeatFinished');
+            }, 0);
+          }
+        }
+      };
+    });;
---
+++ app/new_product/new_product.js
@@ -0,0 +1,81 @@
+'use strict';
+
+angular.module('myApp.new_product', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/new_product', {
+            templateUrl: 'new_product/new_product.html',
+            controller: 'NewProductCtl'
+        });
+    }])
+
+    .controller('NewProductCtl', function($scope, utilFun) {
+
+        utilFun.enableMenuTitle('new-product');
+
+        //$scope.$on('$viewContentLoaded', function(){
+        //
+        //});
+
+
+    })
+    .directive('newProduct', function($http, $q, utilFun,  $timeout){
+        return {
+            restrict: 'E',
+            transclude: true,
+            link: function(scope, element, attrs) {
+
+                scope.loadNewProduct = function(){
+                    //var deferd = $q.defer();
+                    $http.get('mock/new_product.json')
+                        .success(function(response){
+                            scope.images = response;
+
+
+                            if(scope.images.length != 12){
+                                alert('new product only support 12 items');
+                                scope.imageArray = [
+                                    scope.images.slice(0, -1),
+                                ];
+                            }
+                            else {
+                                scope.imageArray = [
+                                    scope.images.slice(0, 5),
+                                    scope.images.slice(5, 9),
+                                    scope.images.slice(9, 12)
+                                ];
+                            }
+
+                            //deferd.resolve('');
+                        }).error(function(e){
+                            alert(e);
+                        });
+                    //return deferd.promise;
+                };
+
+                scope.loadNewProduct();
+
+                scope.renderElement = function(){
+                    $(this).addClass("animated fadeInDown").show();
+                    $(".overlay").hide();
+                    $('.gallery-item').hover(
+                        function() {
+                            $(this).find('.overlay').addClass('animated fadeIn').show();
+                        },
+                        function() {
+                            $(this).find('.overlay').removeClass('animated fadeIn').hide();
+                        }
+                    );
+                    /************** LightBox *********************/
+                    $(function(){
+                        $('[data-rel="lightbox"]').lightbox();
+                    });
+                };
+
+                scope.$on('ngRepeatFinished', function (){
+                    scope.renderElement();
+                });
+            },
+            templateUrl: 'new_product/new_product_template.html'
+        }
+    });
